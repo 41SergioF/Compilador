8a29d8bd79f330bba992c30d2b3eee437a4f74f6
41SergioF <carlossergio2401@outook.com>
2022-12-08 14:46:50 -0300

    Novos tokens

--- example.cs
+++ example.cs
@@ -14,32 +14,31 @@ var num2
 
 num1 = 40
 num2 = 30
-
-IF (num1 > num2){
+if (num1 > num2){
 	num1 = num1 * 3
-	PRINT(num1)
+	Out(num1)
 }ELSE{
-	PRINT(num2)
+	Out(num2)
 }
 	
-	WHILE(num2 < num1){
+	while(num2 < num1){
 		num2 = num2 + 5
-		PRINT(num2)
+		Out(num2)
 	}
 
 var num3
 num3 = 2 ** 5
-PRINT(num3)
+Out(num3)
 
 #var name
-PRINTS("Carlos")
+OutS("Carlos")
 
 num3 = 2
 num3++
-PRINT(num3)
+Out(num3)
 
 var num5
-LER(num5)
-PRINT(num5)
+#Imp(num5)
+#Out(num5)
 
 END
